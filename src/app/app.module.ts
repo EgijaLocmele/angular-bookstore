df2d313eb3544c5ab7f16272694d6f0e8fabea0a
EgijaLocmele <locmele.egija@gmail.com>
2022-06-08 12:12:38 +0300

    First page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductListItemComponent } from './components/product-list-item/product-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProductListComponent,
+    ProductListItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductData } from '../../mock-products';
+import { Product } from '../../product';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.sass'],
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = ProductData;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
